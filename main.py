4ce23ebacd00087a752ddf302f7747de9c73cfc0
feder459 <ff.fede.franco@gmail.com>
2022-05-08 00:46:21 -0300

    Se agrega el ejercicio 1

---
+++ main.py
@@ -0,0 +1,14 @@
+import operaciones
+
+def main():
+    suma=operaciones.suma(2,2)
+    print(suma)
+    resta=operaciones.resta(3,2)
+    print(resta)
+    multiplicacion=operaciones.multiplicacion(5,5)
+    print(multiplicacion)
+    division=operaciones.division(8,2)
+    print(division)
+    
+if __name__ =='__main__':
+    main()
---
+++ operaciones.py
@@ -0,0 +1,10 @@
+def suma(a,b):
+    return a+b
+def resta(a,b):
+    return a-b
+def multiplicacion(a,b):
+    return a*b
+def division(a,b):
+    if (b!=0):
+        resultado=a/b
+    return resultado
